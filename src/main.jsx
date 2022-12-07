85e6c2f69a2aea745efcc008cf07c590b45d295b
DilshanHarendra <harendracripto@gmail.com>
2022-12-07 19:42:34 +0530

    modify main file

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,20 @@ import './index.css'
 import { PublicClientApplication } from "@azure/msal-browser";
 import { MsalProvider } from "@azure/msal-react";
 import { msalConfig } from "../authConfig.js"
+import { BrowserRouter } from 'react-router-dom';
+import {store} from "./store/store.js";
+import {Provider} from "react-redux";
 
 const msalInstance = new PublicClientApplication(msalConfig);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-      <MsalProvider instance={msalInstance}>
-          <App />
-      </MsalProvider>
-  </React.StrictMode>
+    <BrowserRouter>
+        <React.StrictMode>
+            <Provider store={store}>
+                <MsalProvider instance={msalInstance}>
+                    <App />
+                </MsalProvider>
+            </Provider>
+      </React.StrictMode>
+    </BrowserRouter>
 )
