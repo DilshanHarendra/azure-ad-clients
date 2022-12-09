74551e5475b230681e6f8389ba57f25c1b6f6e45
DilshanHarendra <harendracripto@gmail.com>
2022-12-09 19:25:25 +0530

    few change

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,16 @@ import {graphConfig, loginRequest} from "../authConfig.js";
 import {persistor, store} from "./store/store.js";
 import {PersistGate} from 'redux-persist/lib/integration/react';
 import LoadingPage from "./components/LoadingPage.jsx";
-import { useDispatch } from 'react-redux'
-import {setUser} from "./store/reducers/usersSlice.js";
-
+import { useDispatch,useSelector } from 'react-redux'
+import {setUser,setToken,setActiveAccount} from "./store/reducers/usersSlice.js";
+import {activeAccountSelectors} from "./store/selectors/userSelectors.js";
 
 function App() {
     const isAuthenticated= useIsAuthenticated()
     const dispatch = useDispatch()
+    const activeAccount = useSelector(activeAccountSelectors)
 
-    const { instance, accounts } = useMsal();
+    const { instance, accounts,inProgress } = useMsal();
 
     const callMsGraph = (accessToken) => {
         return axios.get(graphConfig.graphMeEndpoint,{
@@ -25,30 +26,44 @@ function App() {
     }
 
 
-    const RequestProfileData = () =>{
+    const requestProfileData = () =>{
         const request = {
             ...loginRequest,
-            account: accounts[0]
+            account: activeAccount
         };
         // Silently acquires an access token which is then attached to a request for Microsoft Graph data
         instance.acquireTokenSilent(request).then((response) => {
+            dispatch(setToken(response.accessToken))
             callMsGraph(response.accessToken).then(response => {
-                dispatch(setUser(response.data))
+                dispatch(setUser(response))
             });
         }).catch((e) => {
             instance.acquireTokenPopup(request).then((response) => {
+                dispatch(setToken(response.accessToken))
                 callMsGraph(response.accessToken).then(response => {
-                    dispatch(setUser(response.data))
+                    dispatch(setUser(response))
                 });
             });
         });
     }
 
     useEffect(()=>{
-        if (isAuthenticated){
-            RequestProfileData()
+        if (isAuthenticated && inProgress == 'none'){
+            const account = instance.getActiveAccount();
+            if (account) {
+                dispatch(setActiveAccount(account))
+            }else{
+                dispatch(setActiveAccount(accounts[0]))
+                console.error("No active account! Verify a user has been signed in and setActiveAccount has been called.");
+            }
         }
-    },[isAuthenticated])
+    },[isAuthenticated,inProgress,instance])
+
+    useEffect(()=>{
+        if (activeAccount){
+            requestProfileData()
+        }
+    },[activeAccount])
 
 
     return (<PersistGate loading={<LoadingPage/>} persistor={persistor}>
