bfba0a6cf4d90672327cefba1b1068e7737a94af
DilshanHarendra <harendracripto@gmail.com>
2022-12-09 19:24:41 +0530

    add active account

--- src/store/reducers/usersSlice.js
+++ src/store/reducers/usersSlice.js
@@ -1,6 +1,8 @@
 import {createSlice} from '@reduxjs/toolkit';
 const init = {
   user: null,
+  token:null,
+  activeAccount:null
 };
 
 const usersSlice = createSlice({
@@ -10,10 +12,16 @@ const usersSlice = createSlice({
     setUser: (state,action) => {
       state.user = action.payload;
     },
+    setToken: (state,action) => {
+      state.token = action.payload;
+    },
+    setActiveAccount: (state,action) => {
+      state.activeAccount = action.payload;
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const {setUser} = usersSlice.actions;
+export const {setUser,setToken,setActiveAccount} = usersSlice.actions;
 
 export default usersSlice.reducer;
