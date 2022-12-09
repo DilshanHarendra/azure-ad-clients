7d19274c152ab222bc02ab2b51c9db594acc7ab0
DilshanHarendra <harendracripto@gmail.com>
2022-12-09 19:24:49 +0530

    add active account selector

--- src/store/selectors/userSelectors.js
+++ src/store/selectors/userSelectors.js
@@ -1,2 +1,3 @@
 import {get} from 'lodash';
 export const userSelectors = state => get(state, 'user.user', null);
+export const activeAccountSelectors = state => get(state, 'user.activeAccount', null);
