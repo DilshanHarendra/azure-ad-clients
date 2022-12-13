c1f81ce79a9087ebb79550eb032bfa948ad6399c
DilshanHarendra <harendracripto@gmail.com>
2022-12-13 18:10:25 +0530

    bind profile data

--- internal-client/src/pages/profile/Profile.jsx
+++ internal-client/src/pages/profile/Profile.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import {useSelector} from "react-redux";
 import {userSelectors} from "../../store/selectors/userSelectors.js";
-
+import {profileEdit} from '../../../authConfig.js'
 const Profile = ()=>{
     const account = useSelector(userSelectors)
 
     return <>
-      <div className="container mx-auto p-5">
+        <div className="container mx-auto p-5">
             <div className="w-96 mx-auto">
                 {
                     account?
-                        <div className="flex flex-col rounded shadow-md bg-white overflow-hidden">
+                        <div className="flex flex-col rounded shadow-md bg-white overflow-hidden relative">
+                            <a href={profileEdit.url} className="absolute right-2 top-2 text-white">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                                </svg>
+
+                            </a>
                             <div className="bg-cover mb-8"
                                  style={{"backgroundImage": `url('https://source.unsplash.com/JgOeRuGD_Y4/800x400')`}}>
                                 <div className="h-32 bg-opacity-25 flex items-end justify-center">
@@ -22,19 +28,16 @@ const Profile = ()=>{
                             </div>
                             <div className="p-5 lg:p-6 flex-grow w-full text-center">
                                 <h3 className="text-lg font-semibold mb-3">
-                                    {account.displayName}
+                                    {account.name}
                                 </h3>
                                 <p className="text-gray-500 text-sm mb-2">
-                                    GivenName ∙ {account.givenName}
+                                    GivenName ∙ {account.given_name}
                                 </p>
                                 <p className="text-gray-500 text-sm mb-2">
-                                    Sure Name ∙ {account.surname || '-'}
+                                    Family Name ∙ {account.family_name || '-'}
                                 </p>
                                 <p className="text-gray-500 text-sm mb-2">
-                                    MobilePhone ∙ {account.mobilePhone || '-'}
-                                </p>
-                                <p className="text-gray-500 text-sm mb-2">
-                                    JobTitle ∙ {account.jobTitle || '-'}
+                                    Scope ∙ {account.scp || '-'}
                                 </p>
                             </div>
                         </div>
@@ -48,7 +51,7 @@ const Profile = ()=>{
                         </div>
                 }
             </div>
-      </div>
+        </div>
     </>
 }
 export default Profile
